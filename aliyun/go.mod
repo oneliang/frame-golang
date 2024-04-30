37112eaf1cbaa66ebdde8d9857cde14e34199685
oneliang <oneliang@oneliang.com>
2024-04-30 10:31:03 +0800

    +aliyun module

---
+++ aliyun/go.mod
@@ -0,0 +1,9 @@
+module github.com/oneliang/frame-golang/aliyun-oss
+
+go 1.22.2
+
+require (
+	github.com/aliyun/aliyun-oss-go-sdk v3.0.2+incompatible // indirect
+	github.com/oneliang/util-golang/file v0.0.0-20240429143549-c3f6e03cbbb9 // indirect
+	golang.org/x/time v0.5.0 // indirect
+)
---
+++ aliyun/oss/client_wrapper.go
@@ -0,0 +1,90 @@
+package aliyunOss
+
+import (
+	"errors"
+	"fmt"
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+	"github.com/oneliang/util-golang/file"
+	"log"
+	"path/filepath"
+)
+
+type ClientWrapper struct {
+	endpoint        string
+	accessKeyId     string
+	accessKeySecret string
+	bucket          string
+	ossClient       *oss.Client
+}
+
+func NewClientWrapper(endpoint string, accessKeyId string, accessKeySecret string, bucket string) (*ClientWrapper, error) {
+	ossClient, err := oss.New(endpoint, accessKeyId, accessKeySecret)
+	if err != nil {
+		log.Fatalf("connect to oss error, endpoint:%s, error:%v", endpoint, err)
+		return nil, err
+	}
+	clientWrapper := &ClientWrapper{
+		endpoint:        endpoint,
+		accessKeyId:     accessKeyId,
+		accessKeySecret: accessKeySecret,
+		bucket:          bucket,
+		ossClient:       ossClient,
+	}
+	return clientWrapper, nil
+}
+func (this *ClientWrapper) uploadFileToOss(objectKey string, filePath string) error {
+	log.Printf("upload file to oss, object key:%s, file path:%s", objectKey, filePath)
+	clientBucket, err := this.ossClient.Bucket(this.bucket)
+	if err != nil {
+		log.Printf("get bucket error, bucket:%s, error%v", this.bucket, err)
+		return err
+	}
+	objectAcl := oss.ObjectACL(oss.ACLPublicRead)
+	if err = clientBucket.PutObjectFromFile(objectKey, filePath, objectAcl); err != nil {
+		log.Printf("put object from file error:%v", err)
+		return err
+	}
+	return nil
+}
+
+func (this *ClientWrapper) UploadFile(objectKey string, filePath string) (string, error) {
+	if !file.Exists(filePath) {
+		errorMessage := fmt.Sprintf("upload file check error, file has not exists:%s", filePath)
+		log.Printf(errorMessage)
+		return "", errors.New(errorMessage)
+	}
+	baseFile := filepath.Base(filePath)
+	if err := this.uploadFileToOss(objectKey, filePath); err != nil {
+		log.Printf("upload file to oss error:%v", err)
+		return "", err
+	}
+	fileUrl := fmt.Sprintf("https://%s.%s/%s",
+		this.bucket,
+		this.endpoint,
+		objectKey,
+		baseFile)
+	return fileUrl, nil
+}
+
+func (this *ClientWrapper) downloadFileFromOss(objectKey string, filePath string) error {
+	log.Printf("download file from oss, object key:%s, file path:%s", objectKey, filePath)
+	clientBucket, err := this.ossClient.Bucket(this.bucket)
+	if err != nil {
+		log.Printf("get bucket error, bucket:%s, error%v", this.bucket, err)
+		return err
+	}
+	objectAcl := oss.ObjectACL(oss.ACLPublicRead)
+	if err = clientBucket.GetObjectToFile(objectKey, filePath, objectAcl); err != nil {
+		log.Printf("get object to file error:%v", err)
+		return err
+	}
+	return nil
+}
+
+func (this *ClientWrapper) DownloadFile(objectKey string, filePath string) error {
+	if err := this.downloadFileFromOss(objectKey, filePath); err != nil {
+		log.Printf("download file from oss error:%v", err)
+		return err
+	}
+	return nil
+}
